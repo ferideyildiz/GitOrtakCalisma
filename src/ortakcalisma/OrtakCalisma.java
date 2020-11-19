f5db85966aa954bf561ff989d244d1621c8176ae
Feride <celikfeza2@gmail.com>
2020-11-19 23:39:31 +0300

    mesaj

--- src/ortakcalisma/OrtakCalisma.java
+++ src/ortakcalisma/OrtakCalisma.java
@@ -6,6 +6,7 @@ public class OrtakCalisma {
 		System.out.println("09:43 - Selamlar");
 		System.out.println("11:16 - feature");
 		System.out.println("11:30 - master");
+		System.out.println("11:38 - feture");
 	}
 
 }
