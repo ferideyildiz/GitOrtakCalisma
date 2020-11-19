69e5e2b0a5e48d427f88518926873dc775b13a90
ferideyildiz <74494589+ferideyildiz@users.noreply.github.com>
2020-11-19 23:40:25 +0300

    Update OrtakCalisma.java

--- src/ortakcalisma/OrtakCalisma.java
+++ src/ortakcalisma/OrtakCalisma.java
@@ -6,6 +6,8 @@ public class OrtakCalisma {
 		System.out.println("09:43 - Selamlar");
 		System.out.println("11:16 - feature");
 		System.out.println("11:30 - master");
+		System.out.println("11:40 - uzak sunucu);
+				   
 	}
 
 }
